89f89d17cd5301dfae7d70218c20a06c389cf674
vinod4848 <vinodku4848@gmail.com>
2023-05-25 18:04:07 +0530

    add port

--- helpers/dbConnect.js
+++ helpers/dbConnect.js
@@ -1,11 +1,14 @@
-const { mongoose } = require("mongoose")
-const dbConnect =()=>{
-try {
-    const conn = mongoose.connect(process.env.MONGODB_URI);
+const { mongoose } = require("mongoose");
+const dbConnect = () => {
+  try {
+    const conn = mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("database is connected successfully");
-} catch (error) {
+  } catch (error) {
     console.log("database is not connected");
-}
+  }
 };
 
 module.exports = dbConnect;
